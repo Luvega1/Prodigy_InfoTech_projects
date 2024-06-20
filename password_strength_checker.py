7a44a30a2705aa2987d3d9bc3c0933201d4a1d11
Lu <130866461+Luvega1@users.noreply.github.com>
2024-06-20 14:00:43 -0400

    Add files via upload
    password strength checker

---
+++ password_strength_checker.py
@@ -0,0 +1,19 @@
+#Password strength checker
+import re
+
+#Password must contain 8 characters
+password = input("Enter Your Password: ")
+if len(password) < 8:
+  print("Password must be at least 8 characters long.")
+  
+  #password contains a uppercase letter
+elif not re.search("[A-Z]", password):
+  print("Password must contain at least one Uppercase letter.")
+   #password contains a lowercase letter
+elif not re.search("[a-z]", password):
+  print("Password must contain at least one lowercase letter.")
+   #password contains a number
+elif not re.search("[0-9]", password):
+  print("Password must contain at least one number..")
+else:
+  print("Password is strong!")
